f346182d49334041e7f93581447e99a7f0aba373
Sowmya Dinavahi and Sean Pierce <sean@>
2017-04-11 09:47:27 -0700

    add business logic to create User Object, instantiate new user, and prototype detemining length of user properties

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
+// create User Object constructor
+function User(name, color) {
+  this.name = name;
+  this.color = color;
+}
+
+// instantiate new user instance
+var newUser = new User("", "");
+
+// prototype to return lengths of User object parameters
+User.prototype.shuffle = function(input) {
+  return input.length;
+}
+
+
+
+
+
+
+
+
+
 $(document).ready(function(){
+
+
 
 });
