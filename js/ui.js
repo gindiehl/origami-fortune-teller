fdd6423f5f4f23a26feede6e3cb1772968f42b70
Sowmya Dinavahi and Sean Pierce <sowmya@>
2017-04-11 16:11:29 -0700

    begin writing animations

--- js/ui.js
+++ js/ui.js
@@ -5,23 +5,25 @@
 
 $(document).ready(function(){
 
-  // instantiate new user object instance
-  var newUser = new User("", "");
+  $("#name-button").click(function() {
 
-  $("#inquire").click(function() {
-    //set variable to collect & store user input for name
-    var userName = $("#name").val();
-    var userColor =$("#color").val();
-    var userFortune = parseInt($("#number").val());
-    newUser.name = userName;
-    newUser.color = userColor;
-    if (shuffle(newUser.name) === 0) {
-      var options = newFortuneTeller.availOptions[0];
-    } else {
-      var options = newFortuneTeller.availOptions[1];
-    }
-    // $("#output").text(newFortuneTeller.fortunes[userFortune -1]);
-    $("#output").text("Available options: " + options);
+    var name = $("#name").val();
+    var times = name.length;
+    var splitName = name.split("");
+
+    (function myLoop (i) {
+      setTimeout(function () {
+        $("#target").addClass("right");
+          setTimeout(function() {
+            $("#target").removeClass("right");
+          }, 300);
+
+          $("#output").text(splitName[(i-times)*-1]);
+
+        if (--i) myLoop(i);
+      }, 600)
+    })(times);
+
 
   });
 });
