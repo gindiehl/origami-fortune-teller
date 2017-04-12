304a42aa3aa35eff83a4781a09df6c906b54ffa5
sowmya dinavahi and Trevor Fast <sowmya@>
2017-04-12 11:11:02 -0700

    refactor backend javascript to be more concise for user input options

--- js/business.js
+++ js/business.js
@@ -25,14 +25,13 @@ function User(name, color) {
   this.color = color;
 }
 
-
 // function to return lengths of User object parameters
-var shuffle = function(input) {
+var shuffle = function(input1,input2) {
   // var timesShuffled = input.length;
-  var number = input.length;
+  var number = input1.length + input2.length;
   if (number % 2 === 0) {
-    return 1;
+    return "even";
   } else {
-    return 0;
+    return "odd";
   }
 }
