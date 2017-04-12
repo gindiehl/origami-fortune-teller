2c6bb4c7d5bbd6aa82507fc55ee7cec2f0293752
Jin Camou and Trevor Fast <trevor@>
2017-04-11 17:30:48 -0700

    add ui to show and hide divs

--- js/ui.js
+++ js/ui.js
@@ -3,25 +3,46 @@
 /////////////////User Logic ////////////////////
 
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-  // instantiate new user object instance
-  var newUser = new User("", "");
+  var newUser = new User("", "");// instantiate new user object instance
 
-  $("#inquire").click(function() {
-    //set variable to collect & store user input for name
-    var userName = $("#name").val();
-    var userColor =$("#color").val();
-    var userFortune = parseInt($("#number").val());
-    newUser.name = userName;
-    newUser.color = userColor;
+  $("#name").click(function() {
+    var inputtedName = $("#userName").val();//collect & store user input for name
+    // newUser.name = inputtedName;
     if (shuffle(newUser.name) === 0) {
       var options = newFortuneTeller.availOptions[0];
     } else {
       var options = newFortuneTeller.availOptions[1];
     }
-    // $("#output").text(newFortuneTeller.fortunes[userFortune -1]);
+    //------------------------------------------shuffle animation
+    $("#show-name").hide();
+    $("#color-div").show();
+    $("#number-div").hide();
+  });
+  //---------------------------------------------------------------color input
+  $("#color").click(function() {
+    var userColor =$("#userColor").val(); //collect ui for color
+    // newUser.color = userColor;
+    if (shuffle(newUser.color) === 0) {
+      var options = newFortuneTeller.availOptions[0];
+    } else {
+      var options = newFortuneTeller.availOptions[1];
+    }
+        //---------------------------------------------shuffle animation
+    $("#color-div").hide();
     $("#output").text("Available options: " + options);
+    $("#number-div").show();
 
   });
+    //----------------------------------------------------------number input
+  $("#number").click(function() {
+    var userNumber = parseInt($("#userNumber").val()); //
+    // newUser.number = userNumber;
+    alert(newFortuneTeller.fortunes[userNumber-1]);
+
+    // $("#output").text(newFortuneTeller.fortunes[userFortune -1]);
+
+  });
+
 });
